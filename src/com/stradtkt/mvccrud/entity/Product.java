381a1f48b97e263a5137a97a37a3b5e6f941c91d
Kevin Stradtman <stradtkt22@gmail.com>
2020-10-07 00:15:10 -0400

    Added the store and product class

--- src/com/stradtkt/mvccrud/entity/Product.java
+++ src/com/stradtkt/mvccrud/entity/Product.java
@@ -1,5 +1,87 @@
 package com.stradtkt.mvccrud.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="store")
 public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="productId")
+	private int id;
+	@Column(name="product_name")
+	private String productName;
+	@Column(name="product_description")
+	private String productDescription;
+	@Column(name="product_price")
+	private float productPrice;
+	@Column(name="product_on_sale")
+	private boolean productOnSale;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "storeId", nullable = false)
+	private Store store;
 	
+	public Product() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductDescription() {
+		return productDescription;
+	}
+
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public float getProductPrice() {
+		return productPrice;
+	}
+
+	public void setProductPrice(float productPrice) {
+		this.productPrice = productPrice;
+	}
+
+	public boolean isProductOnSale() {
+		return productOnSale;
+	}
+
+	public void setProductOnSale(boolean productOnSale) {
+		this.productOnSale = productOnSale;
+	}
+
+	public Store getStore() {
+		return store;
+	}
+
+	public void setStore(Store store) {
+		this.store = store;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", productName=" + productName + ", productDescription=" + productDescription
+				+ ", productPrice=" + productPrice + ", productOnSale=" + productOnSale + ", store=" + store + "]";
+	}
 }
--- src/com/stradtkt/mvccrud/entity/Store.java
+++ src/com/stradtkt/mvccrud/entity/Store.java
@@ -1,5 +1,63 @@
 package com.stradtkt.mvccrud.entity;
 
-public class Store {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="store")
+public class Store {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="storeId")
+	private int id;
+	@Column(name="store_name")
+	private String storeName;
+	@Column(name="store_city")
+	private String storeCity;
+	@Column(name="store_zip")
+	private String storeZip;
+	
+	public Store() {}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStoreName() {
+		return storeName;
+	}
+
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+
+	public String getStoreCity() {
+		return storeCity;
+	}
+
+	public void setStoreCity(String storeCity) {
+		this.storeCity = storeCity;
+	}
+
+	public String getStoreZip() {
+		return storeZip;
+	}
+
+	public void setStoreZip(String storeZip) {
+		this.storeZip = storeZip;
+	}
+
+	@Override
+	public String toString() {
+		return "Store [id=" + id + ", storeName=" + storeName + ", storeCity=" + storeCity + ", storeZip=" + storeZip
+				+ "]";
+	}
 }
